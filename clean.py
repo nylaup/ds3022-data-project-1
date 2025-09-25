d7bff7460b26307ff91bb9719c8eebb44059390f
Nylaup <nylaupal@gmail.com>
2025-09-24 23:30:59 -0400

    updated cleaning.py

--- clean.py
+++ clean.py
@@ -1,2 +1,77 @@
 import duckdb
 import logging
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='clean.log'
+)
+logger = logging.getLogger(__name__)
+
+def clean_data():
+
+    con = None
+
+    try:
+        # Connect to local DuckDB instance
+        con = duckdb.connect(database='emissions.duckdb', read_only=False)
+        logger.info("Connected to DuckDB instance")
+
+        con.execute(f"""
+            -- Remove duplicate trips
+            CREATE TABLE yellow_tripdata_clean AS
+            SELECT DISTINCT * FROM yellow_tripdata;
+
+            DROP TABLE yellow_tripdata;
+            ALTER TABLE yellow_tripdata_clean RENAME TO yellow_tripdata;
+        """)
+        logger.info("Removed duplicate trips")
+        print("Removed duplicate trips")
+
+        con.execute(f"""
+            -- Remove trips with 0 passengers 
+            DELETE FROM yellow_tripdata WHERE passenger_count = 0;
+        """)
+        logger.info("Removed trips with no passengers")
+        print("Removed trips with no passengers")
+
+        con.execute(f"""
+            -- Remove trips 0 miles
+            DELETE FROM yellow_tripdata WHERE trip_distance = 0;
+        """)
+        logger.info("Removed trips with 0 miles")
+        print("Removed trips with 0 miles")
+
+        con.execute(f"""
+            -- Remove trips over 100 miles
+            DELETE FROM yellow_tripdata WHERE trip_distance > 100;
+        """)
+        logger.info("Removed trips over 100 miles")
+        print("Removed trips over 100 miles")
+
+        con.execute(f"""
+            -- Remove trips over 24 hours
+            ALTER TABLE yellow_tripdata 
+            ADD COLUMN trip_duration INTEGER;
+
+            UPDATE yellow_tripdata 
+            SET trip_duration = date_diff('seconds', dropoff_datetime, pickup_datetime);
+                    
+            DELETE FROM yellow_tripdata WHERE trip_duration > 86400;
+        """)
+        logger.info("Removed trips over 24 hours")
+        print("Removed trips over 24 hours")
+
+        count = con.execute(f"""
+            -- SQL goes here 
+            SELECT COUNT(*) FROM yellow_tripdata;
+        """)
+        yellow_count = count.fetchone()[0]
+        print(f"{yellow_count} records in yellow table after cleaning")
+        logger.info(f"{yellow_count} records in yellow table after cleaning")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    clean_data()
--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import duckdb
 import os
 import logging
+import time
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -17,15 +18,17 @@ def load_parquet_files():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
 
+        #YELLOW TAXI
         con.execute(f"""
             -- Drop if existing and create new table
             DROP TABLE IF EXISTS yellow_tripdata;
             CREATE TABLE yellow_tripdata(
-                trip_distance double(100),
-                pickup_datetime timestamp(100)
-            );
+                trip_distance double,
+                pickup_datetime timestamp,
+                dropoff_datetime timestamp,
+                passenger_count integer);
         """)
-        logger.info("Dropped table if exists")
+        logger.info("Dropped yellow table if exists")
 
         yellow_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-{:02d}.parquet"
         for month in range(1,13):
@@ -33,23 +36,59 @@ def load_parquet_files():
             con.execute(f"""
             -- Insert data from each month
             INSERT INTO yellow_tripdata
-                SELECT trip_distance, tpep_pickup_datetime 
+                SELECT trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count
                 FROM read_parquet('{url}');
             """)
             logger.info(f"Added month {month} data to yellow table")
+            time.sleep(60)
 
         count = con.execute(f"""
             -- SQL goes here 
             SELECT COUNT(*) FROM yellow_tripdata;
         """)
-        print(f"Number of records in yellow taxi data: {count.fetchone()[0]}")
-        logger.info(f"Number of records in yellow taxi data: {count.fetchone()[0]}")
+        yellow_count = count.fetchone()[0]
+        print(f"Number of records in yellow taxi data: {yellow_count}")
+        logger.info(f"Number of records in yellow taxi data: {yellow_count}")
 
+        #GREEN TAXI
+        con.execute(f"""
+            -- Drop if existing and create new table
+            DROP TABLE IF EXISTS green_tripdata;
+            CREATE TABLE green_tripdata(
+                trip_distance double,
+                pickup_datetime timestamp,
+                dropoff_datetime timestamp,
+                passenger_count integer);
+        """)
+        logger.info("Dropped green table if exists")
+
+        green_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2024-{:02d}.parquet"
+        for month in range(1,13):
+            url = green_url.format(month)
+            con.execute(f"""
+            -- Insert data from each month
+            INSERT INTO green_tripdata
+                SELECT trip_distance, lpep_pickup_datetime, lpep_dropoff_datetime, passenger_count
+                FROM read_parquet('{url}');
+            """)
+            logger.info(f"Added month {month} data to green table")
+            time.sleep(60)
+
+        count = con.execute(f"""
+            -- SQL goes here 
+            SELECT COUNT(*) FROM green_tripdata;
+        """)
+        green_count = count.fetchone()[0]
+        print(f"Number of records in green taxi data: {green_count}")
+        logger.info(f"Number of records in green taxi data: {green_count}")
+
+        #EMISSIONS TABLE
         con.execute(f"""
             DROP TABLE IF EXISTS emissions_lookup;
-            CREATE TABLE emisssions_lookup AS 
+            CREATE TABLE emissions_lookup AS 
                 SELECT * FROM read_csv('data/vehicle_emissions.csv');
         """)
+        logger.info("Created emissions lookup table")
 
     except Exception as e:
         print(f"An error occurred: {e}")
