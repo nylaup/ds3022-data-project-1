0b82f522bf03f814fb582bec5096aba1822e8e46
Nylaup <nylaupal@gmail.com>
2025-09-25 19:28:33 -0400

    updates to transform

--- transform.py
+++ transform.py
@@ -27,30 +27,55 @@ def transform_data():
                 FROM emissions_lookup e
                 WHERE e.vehicle_type = 'yellow_taxi'; 
         """)
-        logger.info("")
+        logger.info("Added column for emissions per trip")
 
         con.execute(f"""
             -- Calculate mph per trip
+            ALTER TABLE yellow_tripdata
+            ADD COLUMN avg_mph DOUBLE;
+                    
+            UPDATE yellow_tripdata
+            SET avg_mph = (trip_distance * 3600) / trip_duration;
         """)
-        logger.info("")
+        logger.info("Added column for trip mph")
 
         con.execute(f"""
             -- Calculate pickup hour
+            ALTER TABLE yellow_tripdata
+            ADD COLUMN hour INT;
+
+            UPDATE yellow_tripdata
+            SET hour = EXTRACT(HOUR FROM pickup_datetime);
         """)
-        logger.info("")
+        logger.info("Added column for pickup hour")
 
         con.execute(f"""
             -- Calculate trip day of the week
+            ALTER TABLE yellow_tripdata
+            ADD COLUMN day_of_week TEXT;
+                    
+            UPDATE yellow_tripdata
+            SET day_of_week = EXTRACT(DAYNAME(dayofweek FROM pickup_datetime));
         """)
-        logger.info("")
+        logger.info("Added column for week of year")
 
         con.execute(f"""
             -- Calculate week number 
+            ALTER TABLE yellow_tripdata
+            ADD COLUMN week_of_year INTEGER;
+                    
+            UPDATE yellow_tripdata
+            SET week_of_year = EXTRACT(WEEK FROM pickup_datetime);
         """)
-        logger.info("")
+        logger.info("Added column for week of year")
 
         con.execute(f"""
             -- Calculate month
+            ALTER TABLE yellow_tripdata
+            ADD COLUMN month INT;
+
+            UPDATE yellow_tripdata
+            SET month = EXTRACT(MONTH FROM pickup_datetime);
         """)
         logger.info("")
 
