8483f37d78208088db28f1c9f6098c3b368cb465
Nylaup <nylaupal@gmail.com>
2025-09-27 15:57:51 -0400

    add dbt files

---
+++ dbt/models/trip_co2.sql
@@ -0,0 +1,7 @@
+ALTER TABLE yellow_tripdata 
+ADD COLUMN trip_co2_kgs DOUBLE;
+                    
+UPDATE yellow_tripdata
+SET trip_co2_kgs = yellow_tripdata.trip_distance * e.co2_grams_per_mile / 1000
+    FROM emissions_lookup e
+    WHERE e.vehicle_type = 'yellow_taxi'; 
